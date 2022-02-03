9604b93226675c0e071f318d0ee35e901d171da8
armandnjapou <armand2ngassu@gmail.com>
2022-02-03 17:19:35 +0100

    Add constants

---
+++ src/main/java/com/safetynet/alerts/utils/Constants.java
@@ -0,0 +1,23 @@
+package com.safetynet.alerts.utils;
+
+public class Constants {
+    
+    public static final String PERSONS = "persons";
+    public static final String MEDICAL_RECORDS = "medicalrecords";
+    public static final String FIRE_STATIONS = "firestations";
+    public static final String JSON_PATH = "src/main/resources/static/data.json";
+
+    public static final String FIRSTNAME = "firstName";
+    public static final String LASTNAME = "lastName";
+    public static final String ADDRESS = "address";
+    public static final String CITY = "city";
+    public static final String ZIP = "zip";
+    public static final String PHONE = "phone";
+    public static final String EMAIL = "email";
+
+    public static final String STATION = "station";
+
+    public static final String BIRTHDATE = "birthdate";
+    public static final String MEDICATIONS = "medications";
+    public static final String ALLERGIES = "allergies";
+}
